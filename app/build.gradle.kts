b6e49d2f0682539fce50262aab6fca31f87ba4bb
arun-dh <arun.s@dedalus.eu>
2026-08-12 06:21:02 +0530

    constraint layout samples

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    someFun("android")
 }
 
 dependencies {
@@ -46,3 +47,12 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
+tasks.register("helloAndroid") {
+    doLast {
+        println("heyy")
+        someFun("task")
+    }
+}
+
+fun someFun(callSite: String) { println("somefun: $callSite") }
